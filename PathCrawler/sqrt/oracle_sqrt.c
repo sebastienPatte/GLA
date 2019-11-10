f8f04575793e9a59bdc2c6f41322c0d7589a5e13
sebastienPatte <sebnb91@gmail.com>
2019-11-10 16:32:01 +0100

    debut Ex1 : sqrt

--- PathCrawler/sqrt/oracle_sqrt.c
+++ PathCrawler/sqrt/oracle_sqrt.c
@@ -2,11 +2,29 @@ void oracle_sqrt(
   int Pre_a, int a, 
   int pathcrawler__retres__sqrt)
 {
-  int i = pathcrawler__retres__sqrt;
-
-  /* A remplacer par un vrai verdict */
-  pathcrawler_verdict_unknown();
-
+  	int i = pathcrawler__retres__sqrt;
+  	if(a>0)
+  	{
+		if((i*i <= a) && (a < (i+1)*(i+1)))
+		{
+		  	pathcrawler_verdict_success();	
+		}
+		else
+		{
+			pathcrawler_verdict_failure();
+		}
+  	}
+  	else
+  	{
+  		if(i==0)
+  		{
+  			pathcrawler_verdict_success();		
+  		}
+  		else
+		{
+			pathcrawler_verdict_failure();
+		}	
+  	}
   return;
   
 }
