b6393e9d0fe5da14c4d8509546f9a7bf872886aa
sebastienPatte <leekman91@gmail.com>
2019-11-14 17:47:54 +0100

    debut search [Ex2]

--- PathCrawler/search/oracle_search.c
+++ PathCrawler/search/oracle_search.c
@@ -1,13 +1,28 @@
 void oracle_search(
-  int *Pre_A, int *A, 
-  int Pre_n, int n,
-  int Pre_elem, int elem, 
-  int pathcrawler__retres__search)
+  	int *Pre_A, int *A, 
+  	int Pre_n, int n,
+  	int Pre_elem, int elem, 
+  	int pathcrawler__retres__search)
 {
-  int res = pathcrawler__retres__search;
+  	int res = pathcrawler__retres__search;
 
-  /* A remplacer par un vrai verdict */
-  pathcrawler_verdict_unknown();
+  	if(res >=0){
+		if (A[res]==elem){
+			pathcrawler_verdict_success();	
+		}else{
+			pathcrawler_verdict_failure();
+		}
+	}else{
+		for(int cur=0; cur<n; cur++ ) {
+    		if ( elem == A[cur] ) {
+    			//si l'élément est dans le tableau : echec
+    		  	pathcrawler_verdict_failure();
+    		}
+    	}
+		//si l'élément n'est pas dans le tableau : succès
+    	pathcrawler_verdict_success();	
+	}
+
 
   return;
 }
